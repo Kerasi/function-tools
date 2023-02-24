0534ef2d15feb820bdca7fe5bf0a07b48ba9c045
sephomis <50912699+recofepl@users.noreply.github.com>
2023-02-25 00:23:14 +0800

    修改一些错误

--- js/recordAttributeValues.js
+++ js/recordAttributeValues.js
@@ -2,14 +2,14 @@
  * @description 根据传入的标签属性，获取相对应的值
  * @param {string} htmlTag 
  * @param {string} attributes 
- * @returns Array<string></string>
+ * @returns Array<string>
  */
 function recordAttributeValues(htmlTag, attributes) {
   const imgTags = document.getElementsByTagName(htmlTag);
   const fileValues = [];
   for (let i = 0; i < imgTags.length; i++) {
     const fileValue = imgTags[i].getAttribute(attributes);
-    // May get
+    // Filter null value
     if (fileValue) {
         fileValues.push(fileValue);
     }
