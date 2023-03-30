bc5a074379856602df1ccffb7bc5a9f381ebbeb7
sephomis <50912699+recofepl@users.noreply.github.com>
2023-03-30 22:34:44 +0800

    让函数传参更清晰
    将函数的参数描述补充完整
    采用解构写法让函数参数更清晰

--- js/recordAttributeValues.js
+++ js/recordAttributeValues.js
@@ -1,15 +1,15 @@
 /**
- * @description 根据传入的标签属性，获取浏览器 HTML 节点相对应属性值
- * @param {string} HTMLTag 
- * @param {string} attr 
- * @returns {string[]} attrValues
+ * @description 获取浏览器 HTML 节点相对应属性值
+ * @param {string} HTMLTag 打算获取 HTML 上的标签
+ * @param {string} attr HTML 标签上的属性名
+ * @returns {string[]} attrValues 将标签上属性名的值以数组返回
  */
-function recordAttributeValues(HTMLTag, attr) {
+function recordAttributeValues({ HTMLTag, attr }) {
   const tag = document.getElementsByTagName(HTMLTag);
   const attrValues = [];
   for (let i = 0; i < tag.length; i++) {
     const attrValue = tag[i].getAttribute(attr);
-    // Filter null value
+    // 过滤空值
     if (attrValue) {
       attrValues.push(attrValue);
     }
