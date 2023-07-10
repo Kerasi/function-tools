f85505bc83170b7efb7f9a22c6f756520cec7638
sephomis <50912699+Kerasi@users.noreply.github.com>
2023-07-10 08:51:31 +0800

    修改注释

--- js/fetchWebUrl.js
+++ js/fetchWebUrl.js
@@ -2,18 +2,23 @@
  * 
  * @param {string} matchAttribute 匹配对象的属性
  * @param {string} matchContent 匹配对象的内容
- * @param {string} targetAttribute 目标属性
+ * @param {string} targetAttribute 获取的目标属性
  */
 async function test(matchAttribute = 'initiatorType', matchContent, targetAttribute) {
   const resList = [];
   const errList = [];
 
+  // 获取当前网络已经加载的页面请求
   const entries = window.performance.getEntries();
   
+  // 根据 initiatorType 过滤需要的请求内容
   for (const item of entries) {
+    // 过滤属性的内容相符合
     if (item[matchAttribute] === matchContent) {
       try {
+        // 依据目标属性发送 fetch 请求
         const response = await fetch(item[targetAttribute]);
+        // 把内容格式化成 json 数据
         const res = await response.json();
         resList.push(res);
         console.log(res);
@@ -26,6 +31,7 @@ async function test(matchAttribute = 'initiatorType', matchContent, targetAttrib
 
   console.log('成功结果:', resList);
   console.log('失败结果:', errList);
+  // 把综上请求得来的内容集合，组合成一个对象
   customBookObj(resList)
 }
 
